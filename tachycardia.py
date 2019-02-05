af18e7a51e4359bf393ad2ecd7b1ffd6aea6de1d
everettknudsen <jek42@duke.edu>
2019-02-04 22:49:45 -0500

    Added function to remove leading/trailing punctuation and tests

--- tachycardia.py
+++ tachycardia.py
@@ -1,13 +1,30 @@
-def is_tachycardic():
+import string
 
+
+def is_tachycardic():
     word = input('Give me something that might be tachycardia:')
 
-    remove_spaces(word)
+    no_punctuation = remove_punctuation(word)
+    no_spaces = remove_spaces(no_punctuation)
+    print(lowercase_all(no_spaces))
+
+
+def remove_punctuation(word):
+    res = ""
+    for char in word:
+        if char not in string.punctuation:
+            res += char
+    return res
 
 
 def remove_spaces(word):
     stripped = word.strip()
     return stripped
+
+
+def lowercase_all(word):
+    lower = word.lower()
+    return lower
 
 
 if __name__ == "__main__":
