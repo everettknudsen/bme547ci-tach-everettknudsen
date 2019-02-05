5575f31826b66cd32f35862a8d6fd3311491840c
everettknudsen <jek42@duke.edu>
2019-02-05 13:20:32 -0500

    Added function to check for match to tachycardia and return Boolean

--- tachycardia.py
+++ tachycardia.py
@@ -6,7 +6,8 @@ def is_tachycardic():
 
     no_punctuation = remove_punctuation(word)
     no_spaces = remove_spaces(no_punctuation)
-    print(lowercase_all(no_spaces))
+    all_lower = lowercase_all(no_spaces)
+    print(check_for_match(all_lower))
 
 
 def remove_punctuation(word):
@@ -25,6 +26,14 @@ def remove_spaces(word):
 def lowercase_all(word):
     lower = word.lower()
     return lower
+
+
+def check_for_match(word):
+    if word == 'tachycardic':
+        answer = True
+    else:
+        answer = False
+    return answer
 
 
 if __name__ == "__main__":
