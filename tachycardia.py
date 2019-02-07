8d43f4651a879d6e96a20f5e04287207060c58d6
everettknudsen <jek42@duke.edu>
2019-02-07 09:13:19 -0500

    Reformatted final is_tachycardic function to not have user input
    This was done after getting clarification from Dr. Ward in Lecture.

--- tachycardia.py
+++ tachycardia.py
@@ -1,16 +1,18 @@
-import string
+def main():
+    in_string = input('Give me something:')
+    print(is_tachycardic(in_string))
 
 
-def is_tachycardic():
-    word = input('Input tachycardic information:')
-
+def is_tachycardic(word):
     no_punctuation = remove_punctuation(word)
     no_spaces = remove_spaces(no_punctuation)
     all_lower = lowercase_all(no_spaces)
-    print(string_to_set_and_check(all_lower))
+    final_result = string_to_set_and_check(all_lower)
+    return final_result
 
 
 def remove_punctuation(word):
+    import string
     res = ""
     for char in word:
         if char not in string.punctuation:
@@ -41,4 +43,4 @@ def string_to_set_and_check(word):
 
 
 if __name__ == "__main__":
-    is_tachycardic()
+    main()
--- test_tachycardia.py
+++ test_tachycardia.py
@@ -56,3 +56,27 @@ def test_false_string_to_set_and_check(word, boolean):
     from tachycardia import string_to_set_and_check
     answer = string_to_set_and_check(word)
     assert answer == boolean
+
+
+@pytest.mark.parametrize("word, boolean", [
+    ('    @#$%^,,,.Tachy3ARDIA    ,,,,>>><<<@#$%', True),
+    ('   #$%^&tachycardia    ,,,,,,....::::', True),
+    ('tachYCARd1a    ', True),
+    ('3achycardia     @#$%^&*(', True)
+])
+def test_is_tachycardic_true(word, boolean):
+    from tachycardia import is_tachycardic
+    answer = is_tachycardic(word)
+    assert answer == boolean
+
+
+@pytest.mark.parametrize("word, boolean", [
+    ('    @#$%^,,,.bronchitis    ,,,,>>><<<@#$%', False),
+    ('   #$%^&cardia    ,,,,,,....::::', False),
+    ('gastronomy    ', False),
+    ('radiology     @#$%^&*(', False)
+])
+def test_is_tachycardic_false(word, boolean):
+    from tachycardia import is_tachycardic
+    answer = is_tachycardic(word)
+    assert answer == boolean
